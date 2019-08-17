85149f5b2271388baaa9a350f447599cd2ac5fa6
Johnson Joseph <jhonsonj88@gmail.com>
2019-08-17 22:42:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
+class Driver {
+  constructor(name, startDate){
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
 
+
+    yearsExperienceFromBeginningOf(year){
+        let startYear = this.startDate.getFullYear()
+        return year - startYear
+          }
+}
+
+let eastWest = [
+    '1st Avenue',
+    '2nd Avenue',
+    '3rd Avenue',
+    'Lexington Avenue',
+    'Park',
+    'Madison Avenue',
+    '5th Avenue'
+  ];
+
+
+class Route {
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+  blocksTravelled(){
+    let horizantalDiff = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal))
+    let verticalDiff = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical)
+    return horizantalDiff + verticalDiff
+  }
+
+  estimatedTime(peakTime){
+       if(peakTime){
+           return this.blocksTravelled()/2;
+       }else{
+           return this.blocksTravelled()/3;
+       }
+   }
+}
