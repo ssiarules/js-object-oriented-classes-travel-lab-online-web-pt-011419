76fb823446039716bcb09cd596e9cf7df251b0b8
Johnson Joseph <jhonsonj88@gmail.com>
2019-08-17 22:35:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ class Driver {
   constructor(name, startDate){
     this.name = name
     this.startDate = new Date(startDate)
+  }
 
 
     yearsExperienceFromBeginningOf(year){
         let startYear = this.startDate.getFullYear()
         return year - startYear
           }
-  }
-
 }
 
 
