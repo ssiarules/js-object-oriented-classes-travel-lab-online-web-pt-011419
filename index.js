5c057deeaf9d552f58062db0f03f66081e3d8f57
Johnson Joseph <jhonsonj88@gmail.com>
2019-08-17 22:38:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ class Driver {
           }
 }
 
+let eastWest = [
+    '1st Avenue',
+    '2nd Avenue',
+    '3rd Avenue',
+    'Lexington Avenue',
+    'Park',
+    'Madison Avenue',
+    '5th Avenue'
+  ];
+
 
 class Route {
   constructor(beginningLocation, endingLocation){
